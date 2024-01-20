92c5f5ddf3b3d8e3e1cb1de9534f9f065c9efa78
Daniel Șerbănescu <daniel@serbanescu.dk>
2024-01-20 14:14:28 +0100

    tuxedo: updated configuration.txt

--- tuxedo/configuration.nix
+++ tuxedo/configuration.nix
@@ -19,6 +19,16 @@ in
       "${builtins.fetchTarball "https://github.com/ryantm/agenix/archive/main.tar.gz"}/modules/age.nix"
     ];
 
+  # Nix build settings. @TODO: move to a server machine like contbao1.
+  nix.settings.substituters = [
+    "https://cache.nixos.org"
+    "https://cache.armv7l.xyz/?trusted=1"
+  ];
+  nix.settings.trusted-public-keys = [
+    "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+    "cache.armv7l.xyz-1:kBY/eGnBAYiqYfg0fy0inWhshUo+pGFM3Pj7kIkmlBk="
+  ];
+
   # Agenix secrets.
   age.secrets.localhost-account-daniel-password.file = secrets/localhost-account-daniel-password.age;
   age.secrets.localhost-account-daniel-fullname.file = secrets/localhost-account-daniel-fullname.age;
@@ -28,14 +38,18 @@ in
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  # Linux kernel - Using a stable (non-lts) kernel.
+  # Emulate arm.
+  boot.binfmt.emulatedSystems = [ "armv7l-linux" ];
+
+  # Linux kernel - Using a stable LTS kernel.
+  # Check if the latest kernel is used:
+  # ls -l /run/{booted,current}-system/kernel*
   boot.kernelPackages = pkgs.linuxKernel.packages.linux_6_1;
   boot.extraModulePackages = with config.boot.kernelPackages; [ tuxedo-keyboard nvidia_x11 ];
   boot.blacklistedKernelModules = [
-    "i2c_nvidia_gpu" # https://www.kernel.org/doc/html/latest/i2c/busses/i2c-nvidia-gpu.html
+    # https://www.kernel.org/doc/html/latest/i2c/busses/i2c-nvidia-gpu.html
+    "i2c_nvidia_gpu"
   ];
-  # TODO: Disable because of nonfree license. Then find a solution for HDMI output.
-  hardware.nvidia.modesetting.enable = true;
 
   hardware.pulseaudio.enable = true;
   hardware.pulseaudio.package = pkgs.pulseaudioFull;
@@ -138,8 +152,6 @@ in
     hashedPasswordFile = localhost-account-root-password;
   };
 
-  #services.teamviewer.enable = true;
-
 
   # List packages installed in system profile.
   environment.systemPackages = with pkgs; [
@@ -152,7 +164,8 @@ in
     arduino
     insomnia
     dbeaver
-    nixpkgs-review nix-prefetch
+    nixpkgs-review nix-prefetch nix-serve
+    python3
 
     # Drivers and Firmware.
     hplip hplipWithPlugin
@@ -162,7 +175,8 @@ in
     gnome.gnome-tweaks gnome-network-displays freerdp
     gparted evolutionWithPlugins
     firefox-bin tor-browser-bundle-bin
-    qbittorrent  ungoogled-chromium meld
+    qbittorrent  #ungoogled-chromium
+    meld
     strawberry osdlyrics
     gimp pdfarranger drawio inkscape
     signal-desktop
@@ -171,6 +185,7 @@ in
     czkawka
     protonvpn-gui element-desktop
     filezilla
+    ventoy
     
     go2tv simple-dlna-browser
 
@@ -228,14 +243,18 @@ in
   programs.dconf.enable = true;
   virtualisation.libvirtd.enable = true;
   virtualisation.docker.enable = true;
+  virtualisation.docker.enableNvidia = true;
 
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
   services.openssh.banner = banner;
+  services.openssh.settings = {
+    PubkeyAuthentication = true;
+  };
 
   # Open ports in the firewall.
   networking.firewall.allowedTCPPorts = [
-    22
+    22   # OpenSSH
     3000 # EiskaltDC++
     3001 # EiskaltDC++
     3389 # RDP connections
@@ -245,7 +264,7 @@ in
     6250 # EiskaltDC++ DHT
     50001 # LBRY Wallet
   ];
-  networking.firewall.allowedUDPPorts = [ 22 3389 9003
+  networking.firewall.allowedUDPPorts = [
     3000 # EiskaltDC++
     3001 # EiskaltDC++
     6250 # EiskaltDC++ DHT
@@ -269,15 +288,45 @@ in
     echo (date "+%T - %F")
   '';
 
-  environment.shellAliases = {
-    # change nixos-rebuild to use my own version of nixpkgs.
-    nixos-rebuild = "nixos-rebuild -I nixpkgs=/home/daniel/workspace/projects/nixpkgs --keep-going";
+  # Enable OpenGL
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
   };
 
-  # Keep until https://github.com/NixOS/nixpkgs/issues/230097 gets sorted out.
-  #environment.variables = {
-  #  QT_QPA_PLATFORM = "wayland";
-  #};
+  hardware.nvidia = {
+
+    # Modesetting is required.
+    modesetting.enable = true;
+
+    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+    powerManagement.enable = false;
+    # Fine-grained power management. Turns off GPU when not in use.
+    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+    powerManagement.finegrained = false;
+
+    # Use the NVidia open source kernel module (not to be confused with the
+    # independent third-party "nouveau" open source driver).
+    # Support is limited to the Turing and later architectures. Full list of
+    # supported GPUs is at:
+    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
+    # Only available from driver 515.43.04+
+    # Currently alpha-quality/buggy, so false is currently the recommended setting.
+    open = false;
+
+    # Enable the Nvidia settings menu,
+    # accessible via `nvidia-settings`.
+    nvidiaSettings = true;
+
+    # Optionally, you may need to select the appropriate driver version for your specific GPU.
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
+
+  environment.shellAliases = {
+    # change nixos-rebuild to use my own version of nixpkgs.
+    nixos-rebuild = "nixos-rebuild -I nixpkgs=/home/daniel/workspace/projects/nixpkgs --keep-going --log-format bar-with-logs";
+  };
 
   # Initial version. Consult manual before changing.
   system.stateVersion = "22.05";
