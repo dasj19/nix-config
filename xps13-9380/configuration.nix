3783ed9a1d6f634761f53d4beb5bf0e228d03657
Daniel Șerbănescu <daniel@serbanescu.dk>
2024-08-18 01:27:31 +0200

    xps13-9380: moved the code from fish.shellInit to fish.interactiveShellInit to avoid breaking less

--- xps13-9380/configuration.nix
+++ xps13-9380/configuration.nix
@@ -197,15 +197,14 @@
   users.defaultUserShell = pkgs.fish;
 
   # Fish customizations.
-  programs.fish.shellInit = ''
-    echo "xps13-9380"
-    echo (date "+%T - %F")
-  '';
   programs.fish.interactiveShellInit = ''
     # Forcing true colors.
     set -g fish_term24bit 1
     # Empty fish greeting. @TODO: make it a fish option upstream.
     set -g fish_greeting ""
+    # Pasting hostname and current date.
+    echo "xps13-9380"
+    echo (date "+%T - %F")
   '';
 
   # Cross-shell prompt.
