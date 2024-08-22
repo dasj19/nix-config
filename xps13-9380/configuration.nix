30c86f51022cfc46c18705587f041f4e47f0b0c2
Daniel Șerbănescu <daniel@serbanescu.dk>
2024-08-22 20:31:33 +0200

    xps13-9380: added stylix cursor theme

--- xps13-9380/configuration.nix
+++ xps13-9380/configuration.nix
@@ -324,6 +324,8 @@ in
   stylix.enable = true;
   stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/tokyo-night-dark.yaml";
   stylix.image = "${nixos-artwork.wallpapers.simple-dark-gray}/share/artwork/gnome/nix-wallpaper-simple-dark-gray.png";
+  stylix.cursor.package = pkgs.bibata-cursors;
+  stylix.cursor.name = "Bibata-Modern-Classic";
 
   environment.shellAliases = {
     # Provide sass-embedded from nixos.
