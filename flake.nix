15e3df8be3fe6b94e85b644d99c39538d728e0ad
Daniel Șerbănescu <daniel@serbanescu.dk>
2024-09-04 21:24:39 +0200

    xps13-9380: added basic git settings into home manager

--- flake.nix
+++ flake.nix
@@ -79,6 +79,9 @@
 
           # Optionally, use home-manager.extraSpecialArgs to pass
           # arguments to home.nix
+          home-manager.extraSpecialArgs = {
+            inherit gitSecrets;
+          };
         }
       ];
     };
--- machines/xps13-9380/home.nix
+++ machines/xps13-9380/home.nix
@@ -1,10 +1,17 @@
-{ config, pkgs, ... }:
+{ config, pkgs, gitSecrets, ... }:
 
 {
   home.username = "daniel";
   home.homeDirectory = "/home/daniel";
 
   home.stateVersion = "24.05";
+
+  # Home manager news.
+  news.display = "show";
+
+  programs.git.enable = true;
+  programs.git.userName = gitSecrets.danielFullname;
+  programs.git.userEmail = gitSecrets.danielPersonalEmail;
 
   # Let Home Manager install and manage itself.
   #programs.home-manager.enable = true;
