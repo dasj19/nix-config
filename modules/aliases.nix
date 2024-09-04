d2fe2fdc7f60dafbd3244ffa02d863dc706aa9b3
Daniel Șerbănescu <daniel@serbanescu.dk>
2024-09-04 21:31:54 +0200

    aliases: added two new list aliases for listing git status of files

--- modules/aliases.nix
+++ modules/aliases.nix
@@ -2,6 +2,8 @@
   environment.shellAliases = {
     # Eza for well-known ls aliases, we still keep vanilla ls.
     ll = "eza -lh --icons --grid --group-directories-first";
+    lg = "eza -lh --icons --grid --group-directories-first --git";
     la = "eza -lah --icons --grid --group-directories-first";
+    lag = "eza -lah --icons --grid --group-directories-first --git";
   };
 }
