f6043a1e116659c25ea85c23285b4151778d12d2
Daniel Șerbănescu <daniel@serbanescu.dk>
2024-09-05 00:06:02 +0200

    stylix: updated polarity and app opacity

--- modules/stylix.nix
+++ modules/stylix.nix
@@ -4,6 +4,9 @@
   # Theme settings.
   stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/tokyo-night-dark.yaml";
   stylix.image = "${pkgs.adapta-backgrounds}/share/backgrounds/adapta/tealized.jpg";
+  stylix.polarity = "either";
+  stylix.opacity.applications = 0.8;
+
   # Cursor settings.
   stylix.cursor.package = pkgs.bibata-cursors;
   stylix.cursor.name = "Bibata-Modern-Classic";
@@ -14,4 +17,8 @@
   stylix.fonts.sansSerif.name = "UbuntuMono Nerd Font";
   stylix.fonts.monospace.package = pkgs.fira-code-nerdfont;
   stylix.fonts.monospace.name = "FiraCode Nerd Font";
+
+  
+
+  
 }
