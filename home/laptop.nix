c3b4473c5aaaf1c7a3c52a5df01f2f4e88a9f442
Daniel Șerbănescu <daniel@serbanescu.dk>
2024-09-05 00:05:01 +0200

    home laptop profile: added clipboard-indicator gnome extension

--- home/laptop.nix
+++ home/laptop.nix
@@ -4,4 +4,18 @@
   imports = [
     ./base.nix
   ];
+
+  home.packages = with pkgs; [
+    gnomeExtensions.clipboard-indicator
+  ];
+
+  dconf.enable = true;
+  dconf.settings."org/gnome/shell" = {
+    disable-user-extensions = false;
+
+    # `gnome-extensions list` for a list
+    enabled-extensions = [
+      "clipboard-indicator@tudmotu.com"
+    ];
+  };
 }
