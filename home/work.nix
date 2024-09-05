728b509ffc22376dac0cae7d1f682ec533d703d9
Daniel Șerbănescu <daniel@serbanescu.dk>
2024-09-05 16:29:23 +0200

    home: extracted phpStandards as a variable for multiple uses

--- home/work.nix
+++ home/work.nix
@@ -1,5 +1,9 @@
 { lib, config, pkgs, gitSecrets, ... }:
 
+let
+  phpStandards = "Drupal,DrupalPractice,SlevomatCodingStandard,VariableAnalysis";
+in
+
 {
   imports = [
     ./laptop.nix
@@ -13,11 +17,12 @@
     set PATH $PATH $HOME/.config/composer/vendor/bin
   '';
 
-   programs.vscode.userSettings = {
-    "phpcs.standard" = "Drupal";
+  programs.vscode.userSettings = {
+    "php.format.codeStyle" = "Drupal";
+    "phpcs.standard" = phpStandards;
     "phpcs.executablePath" = "/home/daniel/.config/composer/vendor/bin/phpcs";
     "phpResolver.phpSnifferCommand" = "/home/daniel/.config/composer/vendor/bin/phpcs";
-    "phpResolver.phpStandards" = "Drupal,DrupalPractice";
+    "phpResolver.phpStandards" = phpStandards;
   };
 
 }
