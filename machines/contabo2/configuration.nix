1389dadf86fff4f031af3c6a3386bf48b1df6d0f
Daniel Șerbănescu <daniel@serbanescu.dk>
2024-11-08 18:52:16 +0100

    contabo2: fixed indentation

--- machines/contabo2/configuration.nix
+++ machines/contabo2/configuration.nix
@@ -51,15 +51,15 @@ in
   ];
   mailserver.loginAccounts = {
     "${daniel-email}" = {
-       # For generating new hashed passwords use the following commands.
-       # nix shell -p apacheHttpd
-       # htpasswd -nbB "" "super secret password" | cut -d: -f2 > /hashed/password/file/location
-       hashedPasswordFile = config.sops.secrets.daniel_daniel_email_password.path;
+      # For generating new hashed passwords use the following commands.
+      # nix shell -p apacheHttpd
+      # htpasswd -nbB "" "super secret password" | cut -d: -f2 > /hashed/password/file/location
+      hashedPasswordFile = config.sops.secrets.daniel_daniel_email_password.path;
 
-       # List of email aliases: "username@domain.tld" .
-       aliases = [ "postmaster@${daniel-domain}" "webmaster@${daniel-domain}" ];
-       # Catch all emails from the primary domain.
-       catchAll = [ daniel-domain ];
+      # List of email aliases: "username@domain.tld" .
+      aliases = [ "postmaster@${daniel-domain}" "webmaster@${daniel-domain}" ];
+      # Catch all emails from the primary domain.
+      catchAll = [ daniel-domain ];
     };
   };
 
