d7535b23348253f9191eaf0e17991db6e43d0c40
Daniel Șerbănescu <daniel@serbanescu.dk>
2024-12-10 13:24:06 +0100

    contabo2: configured acme for the firmDomain

--- machines/contabo2/webserver.nix
+++ machines/contabo2/webserver.nix
@@ -5,9 +5,23 @@ let
 in
 
 {
+
   # Using caddy webserver.
   services.caddy.enable = true;
   services.caddy.virtualHosts."www.${firm-domain}".extraConfig = ''
+    redir https://${firm-domain}{uri} permanent
+  '';
+  services.caddy.virtualHosts."${firm-domain}".extraConfig = ''
     respond "Hello, world!"
   '';
+
+  security.acme.certs."${firm-domain}" = {
+      group = config.services.caddy.group;
+
+      domain = "${firm-domain}";
+      extraDomainNames = [ "www.${firm-domain}" ];
+      dnsProvider = "cloudflare";
+      dnsResolver = "1.1.1.1:53";
+      dnsPropagationCheck = true;
+  };
 }
