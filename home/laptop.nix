96d12bee9f5de87b8e4f9e0754ae221e6010c751
Daniel Șerbănescu <daniel@serbanescu.dk>
2025-04-15 19:21:20 +0200

    home: removed the codeium vsix extension and its workarounds

--- home/laptop.nix
+++ home/laptop.nix
@@ -49,47 +49,6 @@
   nixpkgs.config.allowUnfree = true;
 
   programs.vscode.profiles.default.extensions =
-  let
-      # Manually installed extensions.
-      # At the moment stock Codeium does not properly work with the Nix ecosystem
-      # as it requires to download the language server from github.
-      codium-vsix = pkgs.vscode-utils.buildVscodeMarketplaceExtension rec {
-        mktplcRef = {
-          name = "codeium";
-          version = "1.21.10";
-          publisher = "Codeium";
-        };
-        vsix = builtins.fetchurl {
-            url = "https://marketplace.visualstudio.com/_apis/public/gallery/publishers/Codeium/vsextensions/codeium/1.21.10/vspackage";
-            sha256 = "0vqlc88qagd4sq8flwdvybyrrdlmmmn4crc4dqdj3fkrcfnj1m3f";
-          };
-        language-server = builtins.fetchurl {
-          url = "https://github.com/Exafunction/codeium/releases/download/language-server-v1.16.4/language_server_linux_x64.gz";
-          sha256 = "1x35l76ga4f3n73rinhfh5a6dxwz1labhw7smrw7ddzjwmhhs6m7";
-        };
-        
-        nativeBuildInputs = [ pkgs.unzip ];
-        buildInputs = [ pkgs.codeium ];
-        
-        unpackPhase = '' 
-          runHook preUnpack
-          
-          mkdir -p extension/dist
-          unzip ${vsix}
-
-          # This is unpractical, a better solution is needed.
-          ln -s /home/daniel/system/home-manager/codium extension/dist/b1b673a8fb8074357da4b4b8c7597b3ae37cb990
-          #ls -l ${language-server}
-          #gunzip -c ${language-server} > extension/dist/079ca7882d78fc474f5e3839828a14f440d448e1/language_server_linux_x64
-          #chmod a+x,a+w extension/dist/079ca7882d78fc474f5e3839828a14f440d448e1/language_server_linux_x64
-
-          runHook postUnpack
-        '';
-        installPhase = ''
-        
-        '';
-      };
-  in
     with pkgs.vscode-marketplace; with pkgs.vscode-extensions; [
     # Nix language support.
     jnoortheen.nix-ide
@@ -99,9 +58,6 @@
 
     # Editor enhancements.
     oderwat.indent-rainbow
-
-    # AI Assistant.
-    codium-vsix
 
   ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
 
