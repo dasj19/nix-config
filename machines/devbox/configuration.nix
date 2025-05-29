96a1a4667c5069e74993490053c09ff68cb9d089
Daniel Șerbănescu <daniel@serbanescu.dk>
2025-05-29 21:54:55 +0200

    devbox: setup mariadb for development use

--- machines/devbox/configuration.nix
+++ machines/devbox/configuration.nix
@@ -67,6 +67,27 @@
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
 
+  # Enable and configure the Mariadb service.
+  services.mysql.enable = true;
+  services.mysql.package = pkgs.mariadb;
+  services.mysql.dataDir = "/var/lib/mysql";
+  services.mysql.settings = {
+    mysqld = {
+      bind-address = "0.0.0.0";
+      port = 3336;
+
+      # Development server settings.
+      # There are permission issues writing logfiles in /var/log, using /tmp for now.
+      collation-server = "utf8_unicode_ci";
+      character-set-server = "utf8";
+      log-error = "/tmp/mysql_err.log";
+      slow_query_log = 1;
+      slow_query_log_file = "/tmp/mysql_slow.log";
+      long_query_time = 2;
+    };
+  };
+
+
   # Network settings.
   networking.firewall.allowedTCPPorts = [
     22
