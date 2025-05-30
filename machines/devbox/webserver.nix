b5054fbcda7d550e17cdce35eae0000518f073dd
Daniel Șerbănescu <daniel@serbanescu.dk>
2025-05-30 17:20:07 +0200

    devbox: added php81 without error display to power webgrind

--- machines/devbox/webserver.nix
+++ machines/devbox/webserver.nix
@@ -16,14 +16,55 @@ in
       # Created with: openssl req  -nodes -new -x509  -keyout server.key -out server.cert
       tls "/srv/certs/server.cert" "/srv/certs/server.key"
 
+      # Use php81 with production-like settings for development tools.
+      handle /webgrind/* {
+        php_fastcgi unix/${config.services.phpfpm.pools.php81.socket}
+      }
+
       # Deliver files and interpret php.
       root * /srv/www/${devbox-domain}/
       file_server
       php_fastcgi unix/${config.services.phpfpm.pools.php84.socket}
+
   '';
 
   # PHP-FPM pools.
   # https://discourse.nixos.org/t/502-bad-gateway-with-caddy-and-php-fastcgi/25429
+  services.phpfpm.pools."php81" = {
+    user = "caddy";
+    group = "caddy";
+    phpPackage = pkgs.php81;
+    settings = {
+      "listen.owner" = config.services.caddy.user;
+      "pm" = "dynamic";
+      "pm.max_children" = 32;
+      "pm.max_requests" = 500;
+      "pm.start_servers" = 2;
+      "pm.min_spare_servers" = 2;
+      "pm.max_spare_servers" = 5;
+      "catch_workers_output" = true;
+    };
+    phpOptions = ''
+      # Report errors but do no display them.
+      error_reporting=E_ALL
+      display_errors=Off
+      display_startup_errors=Off
+      # Enable error logging to file.
+      error_log=/var/log/php81_errors.log
+      # Unlimited error length.
+      log_errors_max_len=0
+      # Increase memory limit.
+      memory_limit=512M
+      # Increase transfer filesizes.
+      post_max_size=100M
+      upload_max_filesize=100M
+      # Mailcatcher support for php.
+      sendmail_path=/run/current-system/sw/bin/catchmail
+      sendmail_from=mailcatcher@devbox.dev
+    '';
+    phpEnv."PATH" = lib.makeBinPath [ pkgs.php81 ];
+  };
+
   services.phpfpm.pools."php84" = {
     user = "caddy";
     group = "caddy";
@@ -71,6 +112,7 @@ in
     phpEnv."PATH" = lib.makeBinPath [ pkgs.php84 ];
   };
   # Allow php to access the system's /tmp folder instead of systemd isolated tmp.
+  systemd.services.phpfpm-php81.serviceConfig.PrivateTmp = lib.mkForce false;
   systemd.services.phpfpm-php84.serviceConfig.PrivateTmp = lib.mkForce false;
 
   # Mailcatcher.
