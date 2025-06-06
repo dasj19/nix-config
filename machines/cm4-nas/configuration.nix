931c6642f8f8750ab1a92a7decd937774867fd3c
Daniel Șerbănescu <daniel@serbanescu.dk>
2025-06-06 19:14:35 +0200

    cm4-nas: enable NFS server to serve the NAS files

--- machines/cm4-nas/configuration.nix
+++ machines/cm4-nas/configuration.nix
@@ -14,42 +14,77 @@ in
       ./../../profiles/server.nix
     ];
 
-  config = {
-    # Use the extlinux boot loader. (NixOS wants to enable GRUB by default)
-    boot.loader.grub.enable = false;
-    # Enables the generation of /boot/extlinux/extlinux.conf
-    boot.loader.generic-extlinux-compatible.enable = true;
+  # Use the extlinux boot loader. (NixOS wants to enable GRUB by default)
+  boot.loader.grub.enable = false;
+  # Enables the generation of /boot/extlinux/extlinux.conf
+  boot.loader.generic-extlinux-compatible.enable = true;
 
-    # Automount software raid at boot time.
-    boot.swraid.enable = true;
+  # Automount software raid at boot time.
+  boot.swraid.enable = true;
 
-    networking.hostName = "cm4-nas";
-    networking.networkmanager.enable = true;
+  networking.hostName = "cm4-nas";
+  networking.networkmanager.enable = true;
 
-    # Server's time zone.
-    time.timeZone = "Europe/Copenhagen";
+  # Server's time zone.
+  time.timeZone = "Europe/Copenhagen";
 
-    # Underpriviledged user.
-    users.users.daniel = {
-      isNormalUser = true;
-      extraGroups = [ "wheel" ];
-    };
-
-    environment.systemPackages = with pkgs; [
-      git
-      wget
-    ];
-
-    # Open ports in the firewall.
-    networking.firewall.allowedTCPPorts = [ 22 ];
-    # networking.firewall.allowedUDPPorts = [ ... ];
-
-    boot.swraid.mdadmConf = ''
-      MAILADDR=${mdmon-email}
-    '';
-
-    # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
-    system.stateVersion = "24.11";
+  # Underpriviledged user.
+  users.users.daniel = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ];
   };
-}
 
+
+  environment.systemPackages = with pkgs; [
+    git
+    wget
+    iperf
+  ];
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  fileSystems."/export/md0" = {
+    device = "/mnt/md0";
+    options = [ "bind" ];
+  };
+  
+  # Enable NFS.
+  services.nfs.server.enable = true;
+  services.nfs.server.exports = ''
+    /export		10.0.10.0/24(insecure,rw,sync,no_subtree_check,crossmnt,fsid=0)
+    /export/md0		10.0.10.0/24(insecure,rw,sync,no_subtree_check)
+  '';
+  services.nfs.server.lockdPort = 4001;
+  services.nfs.server.mountdPort = 4002;
+  services.nfs.server.statdPort = 4000;
+
+  # Open ports in the firewall.
+  networking.firewall.allowedTCPPorts = [
+    22 # SSH
+    111 # NFS
+    2049 # NFS
+    4000 # NFS
+    4001 # NFS
+    4002 # NFS
+    5201 # iperf
+    20048 # NFS
+  ];
+  networking.firewall.allowedUDPPorts = [
+    111 # NFS
+    2049 # NFS
+    4000 # NFS
+    4001 # NFS
+    20048 # NFS
+    4002 # NFS
+    5201 # iperf
+  ];
+
+  boot.swraid.mdadmConf = ''
+    MAILADDR=${mdmon-email}
+  '';
+
+  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+  system.stateVersion = "24.11";
+
+}
