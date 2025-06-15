a7c144c642a87793e6b567f9afcde350279620b8
Daniel Șerbănescu <daniel@serbanescu.dk>
2025-06-15 12:52:56 +0200

    gnome: excluded a few more gnome packages and disabled pam for gdm

--- modules/gnome.nix
+++ modules/gnome.nix
@@ -13,21 +13,26 @@
 
     # Enable gnome-keyring.
     services.gnome.gnome-keyring.enable = true;
-    # Enable PAM for GDM.
-    security.pam.services.gdm.enable = true;
+    # Disable PAM for GDM.
+    security.pam.services.gdm.enable = false;
 
     # Exclude unnecessary GNOME programs.
     environment.gnome.excludePackages = with pkgs; [
-      gnome-photos
-      geary
-      gnome-music
-      gnome-weather
-      gnome-clocks
-      cheese
-      gnome-tour
-      gnome-connections
-      gnome-logs
-      gnome-maps
+      cheese                  # Simple webcam application, I do not need it.
+      geary                   # Email client, I use Evolution instead.
+      gnome-backgrounds       # Wallpaper manager, I declare wallpapers via stylix.
+      gnome-calendar          # Calendar application, I use Evolution.
+      gnome-clocks            # World clocks, I have no use for this.
+      gnome-connections       # Remote desktop client, I use Remmina.
+      gnome-contacts          # Contacts app, I would use Evolution if a need arises.
+      gnome-logs              # Log visualizer, I don't need it.
+      gnome-maps              # Map application, I use a website instead.
+      gnome-music             # Music player, I use strawberry and tauon.
+      gnome-online-accounts   # Online account manager, I have no use for it.
+      gnome-photos            # Advanced photo organizer, does much more than I need.
+      gnome-tour              # Gnome tour for beginners, I can use the help menu instead.
+      gnome-weather           # Weather service, I never use this.
+      totem                   # Video player, I use vlc.
     ];
 
     # Include gnome-packages as part of system-packages.  
