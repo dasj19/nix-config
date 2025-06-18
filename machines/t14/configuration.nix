6707f0803d486233d640e7a4cbf9eef76dcf1e09
Daniel Șerbănescu <daniel@serbanescu.dk>
2025-06-18 23:06:05 +0200

    locale: centralized internationalization settings

--- machines/t14/configuration.nix
+++ machines/t14/configuration.nix
@@ -21,33 +21,12 @@
   # Set your time zone.
   time.timeZone = "Europe/Copenhagen";
 
-  # Select internationalisation properties.
-  #i18n.defaultLocale = "en_DK.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "da_DK.UTF-8";
-    LC_IDENTIFICATION = "da_DK.UTF-8";
-    LC_MEASUREMENT = "da_DK.UTF-8";
-    LC_MONETARY = "da_DK.UTF-8";
-    LC_NAME = "da_DK.UTF-8";
-    LC_NUMERIC = "da_DK.UTF-8";
-    LC_PAPER = "da_DK.UTF-8";
-    LC_TELEPHONE = "da_DK.UTF-8";
-    LC_TIME = "da_DK.UTF-8";
-  };
-
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
   services.displayManager.gdm.enable = true;
   services.desktopManager.gnome.enable = true;
-
-  # Configure keymap in X11
-#  services.xserver.xkb = {
-#    layout = "es";
-#    variant = "";
-#  };
 
   # Configure console keymap
   console.keyMap = "es";
--- modules/locale.nix
+++ modules/locale.nix
@@ -9,6 +9,17 @@
     i18n.defaultLocale = "en_DK.UTF-8";
     # Support all locales.
     i18n.supportedLocales = [ "all" ];
+    i18n.extraLocaleSettings = {
+      LC_ADDRESS = "da_DK.UTF-8";
+      LC_IDENTIFICATION = "da_DK.UTF-8";
+      LC_MEASUREMENT = "da_DK.UTF-8";
+      LC_MONETARY = "da_DK.UTF-8";
+      LC_NAME = "da_DK.UTF-8";
+      LC_NUMERIC = "da_DK.UTF-8";
+      LC_PAPER = "da_DK.UTF-8";
+      LC_TELEPHONE = "da_DK.UTF-8";
+      LC_TIME = "da_DK.UTF-8";
+    };
 
     environment.systemPackages = with pkgs; [
       hunspell
