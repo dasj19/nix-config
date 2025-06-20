79f75e178715a7dc0db177b35bf41d912b306ed6
Daniel Șerbănescu <daniel@serbanescu.dk>
2025-06-20 17:58:41 +0200

    home: initializaed sgptrc file to have it ready for shell-gpt

--- home/laptop.nix
+++ home/laptop.nix
@@ -23,6 +23,7 @@
     (lib.hm.gvariant.mkTuple [ "xkb" "us" ])
     (lib.hm.gvariant.mkTuple [ "xkb" "ro" ])
   ];
+
   # Display battery percentage on the top bar,
   dconf.settings."org/gnome/desktop/interface".show-battery-percentage = true;
   dconf.settings."org/gnome/shell" = {
@@ -38,6 +39,32 @@
       "kando-integration@kando-menu.github.io"
     ];
   };
+
+  home.file."./.config/shell_gpt/.sgptrc".enable = true;
+  home.file."./.config/shell_gpt/.sgptrc".text = ''
+    DEFAULT_MODEL=ollama/deepseek-coder
+    USE_LITELLM=true
+
+    CHAT_CACHE_PATH=/tmp/chat_cache
+    CACHE_PATH=/tmp/cache
+    CHAT_CACHE_LENGTH=100
+    CACHE_LENGTH=100
+    REQUEST_TIMEOUT=60
+    DEFAULT_COLOR=magenta
+    ROLE_STORAGE_PATH=${config.home.homeDirectory}/.config/shell_gpt/roles
+    DEFAULT_EXECUTE_SHELL_CMD=false
+    DISABLE_STREAMING=false
+    CODE_THEME=dracula
+    OPENAI_FUNCTIONS_PATH=${config.home.homeDirectory}/.config/shell_gpt/functions
+    OPENAI_USE_FUNCTIONS=true
+    SHOW_FUNCTIONS_OUTPUT=false
+    API_BASE_URL=default
+    PRETTIFY_MARKDOWN=true
+    SHELL_INTERACTION=true
+    OS_NAME=auto
+    SHELL_NAME=auto
+    OPENAI_API_KEY=ollama
+  '';
 
   # Setup a burn-my-windows profile.
   home.file."./.config/burn-my-windows/profiles/custom.conf".enable = true;
