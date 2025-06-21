f2563ce6f7b4b2cc600d6691a0c7f95505d6c342
Daniel Șerbănescu <daniel@serbanescu.dk>
2025-06-21 17:11:35 +0200

    aliases: fixed indir and added new short aliases

--- modules/aliases.nix
+++ modules/aliases.nix
@@ -6,8 +6,21 @@
       lg      = "eza -lh --icons --grid --group-directories-first --git";
       la      = "eza -lah --icons --grid --group-directories-first";
       lag     = "eza -lah --icons --grid --group-directories-first --git";
+      # List files sorted by size ascending and descending.
+      lt      = "du -sh * | sort -h";
+      lr      = "du -sh * | sort -rh";
+      # Quick(er) navigation.
+      ".."    = "cd ..";
+      h       = "cd ~";
+      c       = "clear";
+      # Networking. public ip, local ips, open ports, internal ports
+      myip    = "curl http://icanhazip.com";
+      lip     = "ip addr show | grep \"inet\\b\" | awk \'{print $2,$NF}\'";
+      op      = "nmap localhost -p 0-65535";
+      p       = "ping 1.1";
+      ports   = "netstat -tulanp";
       # Create a dir and enter it. https://stackoverflow.com/a/55620350
-      indir   =''_indir(){ mkdir "$1"; cd "$1";}; _indir'';
+      indir   = "function indir; mkdir $argv; cd $argv; end; indir";
     };
   };
 }
