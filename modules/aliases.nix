789c2b87e32da941f4f783018ba9390b840b8e24
Daniel Șerbănescu <daniel@serbanescu.dk>
2025-06-21 13:22:34 +0200

    aliases: added indir alias

--- modules/aliases.nix
+++ modules/aliases.nix
@@ -2,10 +2,12 @@
   config = {
     environment.shellAliases = {
       # Eza for well-known ls aliases, we still keep vanilla ls.
-      ll = "eza -lh --icons --grid --group-directories-first";
-      lg = "eza -lh --icons --grid --group-directories-first --git";
-      la = "eza -lah --icons --grid --group-directories-first";
-      lag = "eza -lah --icons --grid --group-directories-first --git";
+      ll      = "eza -lh --icons --grid --group-directories-first";
+      lg      = "eza -lh --icons --grid --group-directories-first --git";
+      la      = "eza -lah --icons --grid --group-directories-first";
+      lag     = "eza -lah --icons --grid --group-directories-first --git";
+      # Create a dir and enter it. https://stackoverflow.com/a/55620350
+      indir   =''_indir(){ mkdir "$1"; cd "$1";}; _indir'';
     };
   };
 }
