104b7346ed4812060e597a0d22ab39e8632400aa
Daniel Șerbănescu <daniel@serbanescu.dk>
2025-06-23 00:22:49 +0200

    fish: added useful fish plugins
    Added done, z, fzf-fish, autopair, sponge and colored-man-pages

--- modules/fish.nix
+++ modules/fish.nix
@@ -3,8 +3,17 @@
   config = {
     # Required packages.
     environment.systemPackages = with pkgs; [
+      # Binary and dependencies.
       fish
       fastfetch
+
+      # Fish plugins.
+      fishPlugins.done                # Notify when long tasks are done.          Docs: https://github.com/franciscolourenco/done
+      fishPlugins.z                   # Pure-fish z directory jumping.            Docs: https://github.com/jethrokuan/z
+      fishPlugins.fzf-fish            # Augment the CLI with key bindings.        Docs: https://github.com/PatrickF1/fzf.fish
+      fishPlugins.autopair            # Navigate the matching pairs.              Docs: https://github.com/jorgebucaran/autopair.fish
+      fishPlugins.sponge              # Cleans unwanted cli entries from history. Docs: https://github.com/meaningful-ooo/sponge
+      fishPlugins.colored-man-pages   # Brings color to man pages.                Docs: https://github.com/PatrickF1/colored_man_pages.fish
     ];
 
     # Enable fish as the default shell.
