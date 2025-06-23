18cc2522f5a7ea8d00b4d2acd068935c7ff8c720
Daniel Șerbănescu <daniel@serbanescu.dk>
2025-06-23 17:51:02 +0200

    fish: added local package fish-colored-man

---
+++ pkgs/fish-colored-man.nix
@@ -0,0 +1,21 @@
+{
+  lib,
+  buildFishPlugin,
+  fetchFromGitHub,
+}:
+buildFishPlugin {
+  pname = "fish-colored-man";
+  version = "0-unstable-20240416";
+  src = fetchFromGitHub {
+    owner = "decors";
+    repo = "fish-colored-man";
+    rev = "1ad8fff696d48c8bf173aa98f9dff39d7916de0e";
+    hash = "sha256-uoZ4eSFbZlsRfISIkJQp24qPUNqxeD0JbRb/gVdRYlA=";
+  };
+  meta = with lib; {
+    description = "Fish shell plugin to bring colors to man pages";
+    homepage = "https://github.com/decors/fish-colored-man";
+    license = licenses.mit;
+    maintainers = [ maintainers.dasj19 ];
+  };
+}
