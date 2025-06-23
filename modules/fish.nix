b14eb9b2d317b0246f1e4b892213f3eb58f2d285
Daniel Șerbănescu <daniel@serbanescu.dk>
2025-06-23 17:51:48 +0200

    fish: added more fish plugins

--- modules/fish.nix
+++ modules/fish.nix
@@ -6,6 +6,7 @@
       # Binary and dependencies.
       fish
       fastfetch
+      grc
 
       # Fish plugins.
       fishPlugins.done                # Notify when long tasks are done.          Docs: https://github.com/franciscolourenco/done
@@ -13,7 +14,16 @@
       fishPlugins.fzf-fish            # Augment the CLI with key bindings.        Docs: https://github.com/PatrickF1/fzf.fish
       fishPlugins.autopair            # Navigate the matching pairs.              Docs: https://github.com/jorgebucaran/autopair.fish
       fishPlugins.sponge              # Cleans unwanted cli entries from history. Docs: https://github.com/meaningful-ooo/sponge
-      fishPlugins.colored-man-pages   # Brings color to man pages.                Docs: https://github.com/PatrickF1/colored_man_pages.fish
+      #fishPlugins.colored-man-pages   # Brings color to man pages.                Docs: https://github.com/PatrickF1/colored_man_pages.fish
+      fishPlugins.puffer              # Nice expander autocomplete improvement.   Docs: https://github.com/nickeb96/puffer-fish
+      fishPlugins.grc                 # Command colorizer.
+      fishPlugins.fish-you-should-use # Reminder to use present aliases.          Docs: https://github.com/paysonwallach/fish-you-should-use
+      fishPlugins.bass                # Run bash commands in fish.                Docs: https://github.com/edc/bass
+      #fishPlugins.async-prompt        # Make the prompt asynchronous thus faster. Docs: https://github.com/acomagu/fish-async-prompt
+
+      # Local fish plugins. @todo submit upstream.
+      # Brings colors to man pages, this works better than colored-man-pages.
+      (pkgs.callPackage ../pkgs/fish-colored-man.nix {buildFishPlugin = pkgs.fishPlugins.buildFishPlugin; } )
     ];
 
     # Enable fish as the default shell.
