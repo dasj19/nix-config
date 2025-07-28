0d6309a3db004ec8dd62a680b7f033dbfbc0f6dc
Daniel Șerbănescu <daniel@serbanescu.dk>
2025-07-28 14:26:00 +0200

    ai module: changed ollama backend from cuda-powered to cpu-powered

--- modules/ai.nix
+++ modules/ai.nix
@@ -2,20 +2,20 @@
 {
   environment.systemPackages = with pkgs; [
     # gpt4all-cuda    # Open GPT client with CUDA support.
-    ollama-cuda     # Ollama server with CUDA support.
+    ollama          # Ollama server without CUDA support.
     shell-gpt       # ChatGPT/Ollama client.
     tgpt            # ChatGPT client with no need for API keys.
     video2x         # video upscaler with the help of cuda.
   ];
-  # Enable Ollama Cuda as a systemd service.
-  systemd.services.ollama-cuda = {
+  # Enable Ollama Cuda-less as a systemd service.
+  systemd.services.ollama-cpu = {
     enable = true;
     after = [ "network.target" ];
     wantedBy = [ "default.target" ];
-    description = "Ollama Cuda server";
+    description = "Ollama Cuda-less server";
     serviceConfig = {
         Type = "simple";
-        ExecStart = "${pkgs.ollama-cuda}/bin/ollama serve";
+        ExecStart = "${pkgs.ollama}/bin/ollama serve";
         Environment = ''HOME=/home/daniel'';
         Restart = "always";
     };
