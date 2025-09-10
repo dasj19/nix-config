a0be3dfbb5100b31b7c94b7fb4a9b5e2df6e3350
Daniel Șerbănescu <daniel@serbanescu.dk>
2025-09-10 22:22:33 +0200

    contabo1: whitespace fixes for email.nix

--- machines/contabo1/email.nix
+++ machines/contabo1/email.nix
@@ -1,39 +1,38 @@
 { config, gitSecrets, pkgs, lib, ... }:
 
 let
-     fritweb-domain = gitSecrets.fritwebDomain;
+  fritweb-domain = gitSecrets.fritwebDomain;
 in
 
 {
+  # Fetch the email server.
+  imports = [
+    ./../../modules/email-server.nix
+  ];
 
-    # Fetch the email server.
-    imports = [
-      ./../../modules/email-server.nix
-    ];
+  sops.secrets.daniel_fritweb_email_password = {};
 
-    sops.secrets.daniel_fritweb_email_password = {};
-
-    # Setup for the mailserver.
-    mailserver = {
-      fqdn = "mail.${fritweb-domain}";
-      # list of domains in format: [ "domain.tld" ];
-      domains = [
-        "${fritweb-domain}"
-      ]; #import config.age.secrets.mailserver-domains.path;
-      loginAccounts = {
-           # Account name in the form of "username@domain.tld".
-           "daniel@${fritweb-domain}" = {
-              name = "${fritweb-domain}";
-              # Password can be generated running: 'mkpasswd -sm bcrypt'.
-              hashedPasswordFile = config.sops.secrets.daniel_fritweb_email_password.path;
-              # List of aliases in format: [ "username@domain.tld" ].
-              aliases = [
-                "postmaster@${fritweb-domain}"
-                "webmaster@${fritweb-domain}"
-              ];
-          };
+  # Setup for the mailserver.
+  mailserver = {
+    fqdn = "mail.${fritweb-domain}";
+    # list of domains in format: [ "domain.tld" ];
+    domains = [
+      "${fritweb-domain}"
+    ]; #import config.age.secrets.mailserver-domains.path;
+    loginAccounts = {
+      # Account name in the form of "username@domain.tld".
+      "daniel@${fritweb-domain}" = {
+        name = "${fritweb-domain}";
+        # Password can be generated running: 'mkpasswd -sm bcrypt'.
+        hashedPasswordFile = config.sops.secrets.daniel_fritweb_email_password.path;
+        # List of aliases in format: [ "username@domain.tld" ].
+        aliases = [
+          "postmaster@${fritweb-domain}"
+          "webmaster@${fritweb-domain}"
+        ];
       };
     };
+  };
 
   # mailserver.stateVersion = 3;
 
