f9c040e2791f9dee1a2f5affff6097de7509150b
Daniel Șerbănescu <daniel@serbanescu.dk>
2025-11-12 12:38:21 +0100

    flake: getting back to follow nixos-unstable channel to reduce compilation time

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
   description = "The dasj-lab flake";
 
   # Tracking 'nixpkgs-unstable' branch which is usually a couple days behind master.
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/master";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   #inputs.nixpkgs.url = "path:///home/daniel/workspace/projects/linux/nixpkgs";
 
   # Assures compatibility with older version of nix before the version 2.4.
