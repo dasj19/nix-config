a2bd953ea42994545df4bc135bd8fc163b2b5dad
Daniel Șerbănescu <daniel@serbanescu.dk>
2025-11-28 00:34:41 +0100

    aliases: revisit and add documentation

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,10 @@
 # Changelog
 In this file I'll try to mention major changes to the configuration.
 
+2025-11-28:
+- aliases: change gs into gst alias to avoid potential conflict with gs - ghost script.
+- aliases: added documentation.
+
 2025-11-27:
 - hardware: new module to be used on bare-metal installations.
 - hardware: added a working smartd config that sends notification emails.
--- modules/aliases.nix
+++ modules/aliases.nix
@@ -1,3 +1,12 @@
+# Shell aliases module.
+# Provides useful shell aliases for everyday terminal usage.
+# Rules:
+# - only non-conflicting aliases are provided (do not override existing linux commands).
+# - keep aliases simple and easy to remember.
+# - use abbreviations where possible. (only up to 4 letters).
+# - use simple words where possible.
+# - do not assign multiple one letter aliases for the same command.
+# - group related aliases and comment them accordingly.
 { pkgs, ... }:
 {
   config = {
@@ -8,95 +17,96 @@
     # https://opensource.com/article/19/7/bash-aliases
     environment.shellAliases = {
       # Eza for well-known ls aliases, we still keep vanilla ls.
-      ll        = "eza -lh --icons --grid --group-directories-first";
-      lg        = "eza -lh --icons --grid --group-directories-first --git";
-      la        = "eza -lah --icons --grid --group-directories-first";
-      lag       = "eza -lah --icons --grid --group-directories-first --git";
+      ll = "eza -lh --icons --grid --group-directories-first";
+      lg = "eza -lh --icons --grid --group-directories-first --git";
+      la = "eza -lah --icons --grid --group-directories-first";
+      lag = "eza -lah --icons --grid --group-directories-first --git";
       # List files sorted by size ascending and descending. List by mod time.
-      lt        = "du -sh * | sort -h";
-      lr        = "du -sh * | sort -rh";
-      left      = "ls -t -1";
+      lt = "du -sh * | sort -h";
+      lr = "du -sh * | sort -rh";
+      left = "ls -t -1";
       # Change file permissions.
-      cha       = "chmod a+rwx";
-      chr       = "chmod a+r";
-      chx       = "chmod a+x";
-      chw       = "chmod a+w";
+      cha = "chmod a+rwx";
+      chr = "chmod a+r";
+      chx = "chmod a+x";
+      chw = "chmod a+w";
       # Assign ownership to current user, current group.
-      cho       = "chown $(whoami):$(id -gn $(whoami))";
+      cho = "chown $(whoami):$(id -gn $(whoami))";
       # Git shortcuts.
-      gs        = "git status";
-      gsh       = "git stash";
-      ga        = "git add";
-      gc        = "git commit";
-      gch       = "git checkout";
-      gp        = "git push";
-      gu        = "git pull";
-      gd        = "git diff";
-      gl        = "git log";
-      g1        = "git log --oneline";
-      glp       = "git log -p";
-      gf        = "git fetch";
-      gm        = "git merge";
-      gmu       = "git merge upstream/master";
+      gst = "git status";
+      gsh = "git stash";
+      ga = "git add";
+      gc = "git commit";
+      gch = "git checkout";
+      gp = "git push";
+      gu = "git pull";
+      gd = "git diff";
+      gl = "git log";
+      g1 = "git log --oneline";
+      glp = "git log -p";
+      gf = "git fetch";
+      gm = "git merge";
+      gmu = "git merge upstream/master";
       # Quick(er) navigation.
-      ".."      = "cd ..";
-      ".2"      = "cd ../../";
-      ".3"      = "cd ../../../";
-      ".4"      = "cd ../../../../";
-      ".5"      = "cd ../../../../../";
-      h         = "cd ~";
-      b         = "cd -";
-      c         = "clear";
-      r         = "sudo -i";
-      x         = "exit";
+      ".." = "cd ..";
+      ".1" = "cd ..";
+      ".2" = "cd ../../";
+      ".3" = "cd ../../../";
+      ".4" = "cd ../../../../";
+      ".5" = "cd ../../../../../";
+      h = "cd ~"; # home
+      b = "cd -"; # back
+      c = "clear"; # clear terminal
+      r = "sudo -i"; # root shell
+      x = "exit"; # exit shell
       # Utils. background jobs, history shortcuts, simplified list of mounted partitions.
-      j         = "jobs -l";
-      o         = "history";
-      o1        = "history | head -10";
-      o2        = "history | head -20";
-      o3        = "history | head -30";
-      mnt       = ''mount | awk -F " " "{ printf \"%s\t%s\n\",\$1,\$3; }" | column -t | egrep ^/dev/ | sort'';
+      j = "jobs -l";
+      o = "history";
+      o1 = "history | head -10";
+      o2 = "history | head -20";
+      o3 = "history | head -30";
+      mnt = ''mount | awk -F " " "{ printf \"%s\t%s\n\",\$1,\$3; }" | column -t | egrep ^/dev/ | sort'';
       # Info about memory, cpu and gpu.
-      meminfo   = "free -m -l -t -h";
-      cpuinfo   = "lscpu";
-      gpuinfo   = "lshw -C display";
+      meminfo = "free -m -l -t -h";
+      cpuinfo = "lscpu";
+      gpuinfo = "lshw -C display";
       # Processes eating memory, and cpu. Files taking most space. Total used space.
-      psmem     = "ps auxf | sort -nr -k 4 | head -10";
-      pscpu     = "ps auxf | sort -nr -k 3 | head -10";
-      most      = "du -hsx * | sort -rh | head -10";
-      total     = "df -hl --total | grep  -E '(Use(d|%)|total)'";
-      dh        = "df -h";
+      psmem = "ps auxf | sort -nr -k 4 | head -10";
+      pscpu = "ps auxf | sort -nr -k 3 | head -10";
+      most = "du -hsx * | sort -rh | head -10";
+      total = "df -hl --total | grep  -E '(Use(d|%)|total)'";
+      dh = "df -h";
       # Development. Get request headers with and without compression; open DDG in a browser.
-      header    = "curl -I";
-      hd        = "curl -I";
-      zheader   = "curl -I --compressed";
-      zh        = "curl -I --compressed";
-      ff        = "xdg-open https://ddg.gg";
-      ddg       = "xdg-open https://ddg.gg";
+      header = "curl -I";
+      hd = "curl -I";
+      zheader = "curl -I --compressed";
+      zh = "curl -I --compressed";
+      ff = "xdg-open https://ddg.gg";
+      ddg = "xdg-open https://ddg.gg";
       # Networking. public ip, local ips, open ports, internal ports, speed test.
-      myip      = "curl http://icanhazip.com";
-      lip       = "ip addr show | grep \"inet\\b\" | awk \'{print $2,$NF}\'";
-      op        = "nmap localhost -p 0-65535";
-      p         = "ping 1.1";
-      ports     = "netstat -tulanp";
-      t         = "speedtest-cli";
-      f         = "sudo iptables --list-rules";
-      firewall  = "sudo iptables --list-rules";
+      myip = "curl http://icanhazip.com";
+      lip = "ip addr show | grep \"inet\\b\" | awk \'{print $2,$NF}\'";
+      op = "nmap localhost -p 0-65535";
+      p = "ping 1.1";
+      ports = "netstat -tulanp";
+      t = "speedtest-cli";
+      f = "sudo iptables --list-rules";
+      firewall = "sudo iptables --list-rules";
       # Create a dir and enter it. Change dir and list files.
       # @see https://stackoverflow.com/a/55620350
-      indir   = "function indir; mkdir $argv; cd $argv; end; indir";
-      cl      = "function cl; cd $argv; ls; end; cl";
+      indir = "function indir; mkdir $argv; cd $argv; end; indir";
+      cl = "function cl; cd $argv; ls; end; cl";
       # Nix specific. OS update. Flake update. Clean nix store.
-      osup    = "sudo nixos-rebuild switch --flake .#$(hostname) --print-build-logs";
-      nhup    = "nh os switch .#nixosConfigurations.$(hostname)"; # calls sudo when needed.
-      flup    = "nix flake update";
-      nhcl    = "nh clean all --keep-since 5d --keep 3";
+      osup = "sudo nixos-rebuild switch --flake .#$(hostname) --print-build-logs";
+      nhup = "nh os switch .#nixosConfigurations.$(hostname)"; # calls sudo when needed.
+      flup = "nix flake update";
+      nhcl = "nh clean all --keep-since 5d --keep 3";
     };
 
     environment.systemPackages = with pkgs; [
-      curl          # Command line tool for transferring data from an URL.
-      nettools      # Contains netstat.
-      nh            # Nix helper - manage nixos and home-manager configurations.   
+      curl # Command line tool for transferring data from an URL.
+      nettools # Contains netstat.
+      nh # Nix helper - manage nixos and home-manager configurations.
       speedtest-cli # Test internet bandwidth speed from the command line.
     ];
   };
