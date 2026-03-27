5653e41a0c02f4b71b53304b155ac062a0e451db
Daniel Șerbănescu <daniel@serbanescu.dk>
2026-03-27 14:26:34 +0100

    tuxedo-xa15: removed virtualbox

--- machines/tuxedo-xa15/configuration.nix
+++ machines/tuxedo-xa15/configuration.nix
@@ -234,13 +234,6 @@ in
   virtualisation.docker.enableOnBoot = false;
   virtualisation.waydroid.enable = true;
 
-  # Enable virtualbox.
-  virtualisation.virtualbox.host.enable = true;
-  virtualisation.virtualbox.host.enableExtensionPack = true;
-  users.users.daniel.extraGroups = [
-    "vboxusers"
-  ];
-
   services.mysql.enable = true;
   services.mysql.package = pkgs.mariadb;
 
@@ -258,10 +251,6 @@ in
     # Graphic drivers.
     "nvidia-x11"
     "nvidia-settings"
-
-    # Virtualization.
-    "virtualbox-extpack"
-    "virtualbox"
 
     # AI stuff.
     "open-webui"
