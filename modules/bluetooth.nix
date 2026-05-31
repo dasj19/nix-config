7f07ef2f23e72ada2847772e1cf90e8919a4f70a
Daniel Șerbănescu <daniel@serbanescu.dk>
2026-05-31 22:59:30 +0200

    bluetooth: add file header

--- modules/bluetooth.nix
+++ modules/bluetooth.nix
@@ -1,4 +1,9 @@
-_: {
+# bluetooth: settings for bluetooth
+# scope: desktop
+
+_:
+
+{
   hardware.bluetooth.enable = true;
   hardware.bluetooth.powerOnBoot = false;
   services.blueman.enable = true;
