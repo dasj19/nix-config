4a7176412e96cfe3059292776067cae0ffe57e50
Daniel Șerbănescu <daniel@serbanescu.dk>
2026-05-31 22:58:50 +0200

    audio: reorganize code and add comments

--- modules/audio.nix
+++ modules/audio.nix
@@ -1,13 +1,17 @@
+# audio: system-wide sound settings.
+# scope: desktop
+
+_:
 {
-  config = {
-    # Enable sound with pipewire.
-    services.pulseaudio.enable = false;
-    services.pipewire.enable = true;
-    security.rtkit.enable = true;
-    services.pipewire.audio.enable = true;
-    services.pipewire.pulse.enable = true;
-    services.pipewire.jack.enable = true;
-    services.pipewire.alsa.enable = true;
-    services.pipewire.alsa.support32Bit = true; 
-  };
+  # Pipewire can acquire real time priority with rtkit.
+  security.rtkit.enable = true;
+  # Disable pulseaudio.
+  services.pulseaudio.enable = false;
+  # Enable sound with pipewire.
+  services.pipewire.enable = true;
+  services.pipewire.audio.enable = true;
+  services.pipewire.pulse.enable = true;
+  services.pipewire.jack.enable = true;
+  services.pipewire.alsa.enable = true;
+  services.pipewire.alsa.support32Bit = true;
 }
