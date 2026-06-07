782a2e98805cd0f73e8045c501f5adff069dd645
Daniel Șerbănescu <daniel@serbanescu.dk>
2026-06-07 13:32:06 +0200

    ai: use cuda packages for ollama

--- modules/ai.nix
+++ modules/ai.nix
@@ -7,6 +7,44 @@
 }:
 
 {
+
+  imports = [
+    # Non-free software.
+    ./non-free.nix
+  ];
+
+  # Non-free AI/Cuda libraries.
+  allowedUnfree = [
+    "cuda_cccl"
+    "cuda_cudart"
+    "cuda_cuobjdump"
+    "cuda_cupti"
+    "cuda_cuxxfilt"
+    "cuda_gdb"
+    "cuda-merged"
+    "cuda_nvcc"
+    "cuda_nvdisasm"
+    "cuda_nvml_dev"
+    "cuda_nvprune"
+    "cuda_nvrtc"
+    "cuda_nvtx"
+    "cuda_profiler_api"
+    "cuda_sanitizer_api"
+    "cudnn" # cspell:disable-line
+    "libcublas"
+    "libcurand"
+    "libcusparse"
+    "libnvjitlink"
+    "libcufile" # cspell:disable-line
+    "libcufft"
+    "libcusolver"
+    "libcusparse_lt"
+    "libnpp"
+    # Self-hosted web solutions.
+    "open-webui"
+  ];
+
+  # AI system tools.
   environment.systemPackages = with pkgs; [
     llmfit      # Checks what LLMs run optimally on current hardware.
     shell-gpt   # ChatGPT/Ollama client.
@@ -14,8 +52,14 @@
     video2x     # video upscaler with cuda support.
   ];
 
+  # Use cuda-powered ollama.
   services.ollama.enable = true;
   services.ollama.user = "ollama";
+  services.ollama.package = pkgs.ollama-cuda;
+  # Prefer cuda packages from nixpkgs.
+  # This triggers rebuilding of massive software packages
+  # like GIMP and the like, we avoid it for now.
+  #nixpkgs.config.cudaSupport = true;
 
   # cspell:ignore gguf
   # llama-cpp supports models in the gguf format.
